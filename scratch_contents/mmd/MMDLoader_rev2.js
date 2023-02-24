e2f8cc85eb41ded29cbf5b93123349053ef11066
m-asyua <83494645+m-asyua@users.noreply.github.com>
2023-02-25 00:29:55 +0900

    Update MMDLoader_rev2.js

--- scratch_contents/mmd/MMDLoader_rev2.js
+++ scratch_contents/mmd/MMDLoader_rev2.js
@@ -1352,7 +1352,7 @@ console.log("MMDLoader_rev2:",fullPath,  this.resourcePath , filePath);
 
 					if(div_tmp==null){
 						div_tmp        = document.createElement('div');
-						div_tmp.style      = "position:absolute; top:330px ; left:15px";
+						div_tmp.style      = "position:absolute; top:5px ; left:15px";
 						div_tmp.id = "div_message2";
 						let message_id = document.getElementsByClassName("stage_stage-bottom-wrapper_fnczd")[0];
 						message_id.appendChild(div_tmp);
